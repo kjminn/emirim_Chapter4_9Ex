1ebe90f6c9dac8fc6f4e9ca6e4b54d7973590aa7
kjminn <kjminn2001@gmail.com>
2021-05-04 19:50:55 +0900

    Initial commit

---
+++ app/src/main/java/kr/hs/emirim/kjminn/emirim_chapter4_9ex/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.kjminn.emirim_chapter4_9ex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
